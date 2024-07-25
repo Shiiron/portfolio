1269df0f3f4f4cce60f0ce46bfcf772980bc2087
Loris Puech <puech.loris@gmail.com>
2024-07-25 23:53:45 +0200

    added expérience

--- src/app/components/experiences/experiences.component.ts
+++ src/app/components/experiences/experiences.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Experience } from 'src/app/model/experience';
 
 @Component({
   selector: 'app-experiences',
@@ -8,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrl: './experiences.component.scss'
 })
 export class ExperiencesComponent {
+  experiences: Experience[] = [
+    {
+      menu_label: 'Solaris BI',
+      title: 'Développeur Front-End',
+      sub_title: 'Mars 2022 - Octobre 2023',
+      description: `Développement de l'application APOLLO, le logisticiel de gestion logistique pour les PME qui vous accompagne dans la gestion de véhicules et de tournée.
 
+      Responsable du développement Front-End de l'application
+      Création et maintenance des différents composants Angular
+      Intégration des designs et amélioration de l'expérience utilisateur
+      `
+    },
+    {
+      menu_label: 'Synergee',
+      title: 'Développeur Front-End',
+      sub_title: 'Mars 2017 - Mars 2022',
+      description: `Développement de l'application Synergee, logiciel d'aide au pilotage de vos réseaux de franchise
+
+      Responsable du développement Front-End de l'application
+      Création et maintenance des différents composants Angular
+      Intégration des designs et travail sur l'expérience utilisateur
+      `
+    }
+  ];
+  selectedExperience: Experience;
+  selectedIndex: number = 0;
+
+  constructor() {
+    this.selectedExperience = this.experiences[0];
+  }
+
+  setSelectedExperience(i: number) {
+    this.selectedExperience = this.experiences[i];
+    this.selectedIndex = i;
+  }
 }
---
+++ src/app/model/experience.ts
@@ -0,0 +1,6 @@
+export interface Experience {
+  title: string;
+  sub_title: string;
+  menu_label: string;
+  description: string;
+}
